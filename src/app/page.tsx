dcc48a4e05b85ec9288a00c1138c6e0ba97efcce
Daniel <danielchochlisnki@hotmail.com>
2023-06-10 20:16:32 +0200

    form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import styles from "./page.module.scss";
 import HeroView from "@/views/home/hero/HeroView";
 import ProjectsView from "@/views/home/projects/ProjectsView";
 import NavbarView from "@/views/navbar/NavbarView";
+import ContactView from "@/views/home/contact/ContactView";
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
         <ProjectsView />
         <AboutMeView />
       </main>
+      <ContactView />
     </>
   );
 }
---
+++ src/views/home/contact/ContactView.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import styles from "./ContactView.module.scss";
+const ContactView = () => {
+  return <div className={styles.container}></div>;
+};
+
+export default ContactView;
